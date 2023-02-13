d08ac733062e21f05170a6925fb020b5fffb65ee
mlorber <87738509+mlorberdev@users.noreply.github.com>
2023-02-12 19:38:10 -0500

    first commit

---
+++ assets/script/intro.js
@@ -0,0 +1,9 @@
+export function intro() {
+
+  setTimeout(() => {
+    // document.body.style.transition = "transition: background-color var(--t1) linear;";
+    // document.body.style.backgroundColor = "var(--scarlet)";
+    // document.getElementById("name").style.color = "white";
+    // document.getElementById("logo").style.fill = "white";
+  }, 0);
+}
